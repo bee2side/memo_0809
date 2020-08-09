49454fe2d27a5cbd400360907f0acf131e2717f0
bee2side <bee2side@gmail.com>
2020-08-09 20:23:38 +0900

    set tableView

---
+++ memo_0809/Model/Model.swift
@@ -0,0 +1,24 @@
+//
+//  Model.swift
+//  memo_0809
+//
+//  Created by Doris on 2020/08/09.
+//  Copyright © 2020 kakao. All rights reserved.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "Lorem"),
+        Memo(content: "Subscribe")
+    ]
+}
