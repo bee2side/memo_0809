1d32d5cb8caa94774cff3f500765f7cbc08f543c
bee2side <bee2side@gmail.com>
2020-08-09 20:01:58 +0900

    Initial Commit

---
+++ memo_0809/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  memo_0809
+//
+//  Created by Doris on 2020/08/09.
+//  Copyright © 2020 kakao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
