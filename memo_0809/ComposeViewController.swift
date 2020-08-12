7975eaa982ef1c85f6dbd450da18571c3b1f5031
bee2side <bee2side@gmail.com>
2020-08-12 21:41:27 +0900

    newMemo View

---
+++ memo_0809/ComposeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ComposeViewController.swift
+//  memo_0809
+//
+//  Created by Doris on 2020/08/12.
+//  Copyright © 2020 kakao. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
